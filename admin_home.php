c33279c136c311941e98c3252162f14ad8c217aa
KoushikAhamed <35923187+KoushikAhamed@users.noreply.github.com>
2018-03-14 03:07:02 +0600

    Update admin_home.php

--- admin_home.php
+++ admin_home.php
@@ -1,1 +1,30 @@
-<?phpsession_start();if(!isset($_SESSION['login_user']) ){ header("location: login.php"); }?><center>	<h1>Welcome <?php	$servername ="localhost";	$username 	="root";	$password 	="";	$dbname 	="setb";		$id=$_SESSION['login_user'];    $conn = mysqli_connect($servername, $username, $password, $dbname);	$sql = "select name from user where id='$id'" ;		$result=mysqli_query($conn, $sql );		$arr=mysqli_fetch_assoc($result);		echo $arr['name'];		mysqli_close($conn);	?></h1>	<a href="profile.php">Profile</a>	<br/>	<a href="change_password.php">Change Password</a>	<br/>	<a href="view_users.php">View Users</a>	<br/>	<a href="login.php">Logout</a></center>
+<?php
+session_start();
+if(!isset($_SESSION['login_user']) )
+{ header("location: login.php");
+ }
+?>
+<center>
+	<h1>Welcome <?php
+	$servername ="localhost";
+	$username 	="root";
+	$password 	="";
+	$dbname 	="setb";		
+$id=$_SESSION['login_user'];
+  
+  $conn = mysqli_connect($servername, $username, $password, $dbname);
+	$sql = "select name from user where id='$id'" ;
+	
+	$result=mysqli_query($conn, $sql );	
+	$arr=mysqli_fetch_assoc($result);
+	echo $arr['name'];
+	mysqli_close($conn);
+	?></h1>
+	<a href="profile.php">Profile</a>
+	<br/>
+	<a href="change_password.php">Change Password</a>
+	<br/>
+	<a href="view_users.php">View Users</a>
+	<br/>
+	<a href="login.php">Logout</a>
+</center>
